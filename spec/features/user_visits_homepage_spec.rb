3a933e095347a163e291857aa21975d90c366759
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-12 12:44:40 +0700

    homepage feature passing tests

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User visits homepage' do
-  scenario 'successfully' do
+  scenario 'user successfully visits homepage' do
     visit root_path # Capybara
 
     expect(page).to have_css 'h1', text: 'Todos' # Rspec  passing in page as object from Capybara
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,13 +11,10 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs

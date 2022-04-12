9832eeb13c3ac44d9aabddcd9b59e7803eedff8c
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-12 15:25:07 +0700

    refactored to Josh approach

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_12_065951) do
+ActiveRecord::Schema.define(version: 2022_04_12_080400) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "email"
   end
 

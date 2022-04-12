335f6c37926a3a32387af8854b0dfac9d7470c02
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-12 13:22:07 +0700

    added create todo spec

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature 'User creats todo' do
+RSpec.feature 'User creates todo' do
   scenario 'user successfully creates a todo' do
     todo_title = 'My first todo'
 
     visit root_path
-    click_link 'Add todo'
+    click_link 'Add a new todo'
     fill_in 'Title', with: todo_title
     click_on 'Submit'
 
